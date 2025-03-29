10b8c1f936588abcb05c391d0d60ab1f5da97888
Bedirhan Kerim <bedirhankerim07@gmail.com>
2025-03-29 08:48:05 +0300

    feat: Added character animations

--- BuilderIdleArcadeClone/Assets/Scripts/MovementManager.cs
+++ BuilderIdleArcadeClone/Assets/Scripts/MovementManager.cs
@@ -8,8 +8,8 @@ namespace Game.Core
         [SerializeField] private Joystick joystick;
         [SerializeField] private Rigidbody playerRigidbody;
         [SerializeField] private Transform playerTransform;
-        [SerializeField] private float moveSpeed = 5f; 
-
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private Animator playerAnimator;
 
 
         private void FixedUpdate()
@@ -20,7 +20,7 @@ namespace Game.Core
             Vector3 moveDirection = new Vector3(moveX, 0, moveZ).normalized;
 
             playerRigidbody.velocity = moveDirection * moveSpeed + new Vector3(0, playerRigidbody.velocity.y, 0);
-
+            playerAnimator.SetFloat("Velocity", playerRigidbody.velocity.magnitude);
             if (moveDirection != Vector3.zero)
             {
                 playerTransform.forward = moveDirection;
